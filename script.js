a1f1a66ebe8f423c58e66eef19fe68ff4dd728e0
TspTyhard <59503462+IClapDilex@users.noreply.github.com>
2021-07-14 21:24:22 +0300

    improve code

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 document.getElementById('okstart').onclick = () => {
+    document.getElementById('video').onplaying = () => {
+        document.getElementById('audio').play();
+        setTimeout(() => {
+            document.getElementById('content').removeAttribute('hidden');
+        }, 6000)
+    }
+
     document.getElementById('before').setAttribute('hidden', 'hidden');
     document.getElementById('start').removeAttribute('hidden');
-    document.getElementById('audio').play();
-
-    setTimeout(() => {
-        document.getElementById('content').removeAttribute('hidden');
-    }, 6000)
 
     var countdownDate = 'August 24 2021 20:30:00 GMT+0300';
 
-    var interval = setInterval(() => {
+    setInterval(() => {
         const total = Date.parse(countdownDate) - Date.parse(new Date());
 
         var days = Math.floor( (total/(1000*60*60*24)) );
@@ -21,15 +23,6 @@ document.getElementById('okstart').onclick = () => {
         document.getElementById('hours').innerHTML = hours + ' hours';
         document.getElementById('minutes').innerHTML = minutes + ' minutes ';
         document.getElementById('seconds').innerHTML = seconds + ' seconds ';
-
-        if (timeleft < 0) {
-            clearInterval(interval);
-            document.getElementById('days').innerHTML = ""
-            document.getElementById("hours").innerHTML = "" 
-            document.getElementById("minutes").innerHTML = ""
-            document.getElementById("seconds").innerHTML = ""
-            document.getElementById("end").innerHTML = "ZVNFN IS LAUNCHING SUPER SOON!"
-        }
     }, 1000)
     
 }
